1c3ce62c04d4615fc95239c27dfc9bc328239d4d
Airat Sharifullin <sharifullin.airat@yandex.ru>
2024-05-03 14:45:19 +0300

    stepik

--- 9.5.5.py
+++ 9.5.5.py
@@ -4,3 +4,9 @@
 # Функция должна сортировать по неубыванию список чисел values, делая при этом приоритетной группу чисел из group, которая должна следовать первой.
 # Примечание 1. В тестирующую систему сдайте программу, содержащую только необходимую функцию sort_priority(), но не код, вызывающий ее.
 
+def sort_priority(values, group):
+    values.sort()
+    for i in sorted(group, reverse=True):
+        if i in values:
+            del values[values.index(i)]
+            values.insert(0, i)
