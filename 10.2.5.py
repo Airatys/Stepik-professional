2059d5ed986dbd3929a71d76e10169983d130701
Airat Sharifullin <sharifullin.airat@yandex.ru>
2024-05-12 00:10:20 +0300

    stepik

--- 10.2.5.py
+++ 10.2.5.py
@@ -4,3 +4,24 @@
 # Если итерируемый объект iterable пуст, функция должна вернуть значение None.
 # Примечание 1. В тестирующую систему сдайте программу, содержащую только необходимую функцию get_min_max(), но не код, вызывающий ее.
 
+def get_min_max(iterable):
+    try:
+        min_iter = max_iter = next(iterable)
+        for i in iterable:
+            if i < min_iter:
+                min_iter = i
+            if i > max_iter:
+                max_iter = i
+        return min_iter, max_iter
+    except:
+        try:
+            return min(iterable), max(iterable)
+        except:
+            return None
+ 
+        
+
+    
+data = iter(['bbb'])
+
+print(get_min_max(data))
